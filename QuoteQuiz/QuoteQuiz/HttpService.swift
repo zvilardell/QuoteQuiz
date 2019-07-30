ad9f4de87cc56ab2c7c8cef87dc4fa93dba5afe7
Zach Vilardell <zvilardell@bluebeam.com>
2019-07-30 15:44:57 -0700

    safety

---
+++ QuoteQuiz/QuoteQuiz/HttpService.swift
@@ -0,0 +1,22 @@
+//
+//  HttpService.swift
+//  QuoteQuiz
+//
+//  Created by Zach Vilardell on 7/30/19.
+//  Copyright © 2019 zvilardell. All rights reserved.
+//
+
+import Foundation
+
+class HttpService {
+    
+    let kanyeEndpoint = "https://api.kanye.rest"
+    
+    static let shared = HttpService()
+    private init() {}
+    
+    func getYeQuote() {
+        
+    }
+    
+}
