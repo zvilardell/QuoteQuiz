3bc0130a9d4b8c4fb568da0949f44d81a5d3c22e
Zach Vilardell <zvilardell@bluebeam.com>
2019-07-31 10:36:32 -0700

    move QuizViewController to xib, delete Main.storyboard

--- QuoteQuiz/QuoteQuiz/QuizViewController.swift
+++ QuoteQuiz/QuoteQuiz/QuizViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  QuizViewController.swift
 //  QuoteQuiz
 //
-//  Created by Zach Vilardell on 7/30/19.
+//  Created by Zach Vilardell on 7/31/19.
 //  Copyright © 2019 zvilardell. All rights reserved.
 //
 
@@ -11,6 +11,14 @@ import UIKit
 class QuizViewController: UIViewController {
     
     @IBOutlet weak var quoteTextView: UITextView!
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +29,7 @@ class QuizViewController: UIViewController {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(getQuote))
         view.addGestureRecognizer(tapGestureRecognizer)
     }
-
+    
     @objc func getQuote() {
         HttpService.shared.getRonSwansonQuote() { [weak self] quote in
             DispatchQueue.main.async {
