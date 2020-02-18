1471fa5e0717e92e924755accc8e4035387a1c43
Zach Vilardell <zvilardell@bluebeam.com>
2020-02-18 14:51:04 -0800

    tapping buttons navigates to results view

--- QuoteQuiz/QuoteQuiz/ViewControllers/GameButtonsViewController/SwiftUI/GameButtonsView.swift
+++ QuoteQuiz/QuoteQuiz/ViewControllers/GameButtonsViewController/SwiftUI/GameButtonsView.swift
@@ -9,19 +9,25 @@
 import SwiftUI
 
 struct GameButtonsView: View {
+    
     var body: some View {
-        ZStack {
-            //set background color
-            Color.black
-            .edgesIgnoringSafeArea(.all)
-            
-            //buttons
-            VStack {
-                GameButton(title: "Breaking Bad")
-                GameButton(title: "Ron Swanson")
-                GameButton(title: "Kanye West")
+        
+        NavigationView {
+            ZStack {
+                //set background color
+                Color.black
+                .edgesIgnoringSafeArea(.all)
+                
+                //buttons
+                VStack {
+                    GameButton(title: "Breaking Bad")
+                    GameButton(title: "Ron Swanson")
+                    GameButton(title: "Kanye West")
+                }
             }
         }
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
@@ -34,17 +40,16 @@ struct GameButton: View {
     }
 
     var body: some View {
-        Button(action: {
-            print("Hey")
-        }) {
+        //navigation link only working once in preview/simulator due to Xcode bug
+        NavigationLink(destination: ResultsView()) {
             Text(title)
-                .font(Font.system(size: 15))
-                .foregroundColor(Color.white)
+            .font(Font.system(size: 15))
+            .foregroundColor(Color.white)
+            .frame(minWidth: 0.0, maxWidth: .infinity, minHeight: 55.0, maxHeight: 55.0)
+            .background(Color(red: 170 / 255, green: 86 / 255, blue: 38 / 255))
+            .cornerRadius(5)
+            .padding([.vertical])
         }
-        .frame(minWidth: 0.0, maxWidth: .infinity, maxHeight: 55.0)
-        .background(Color(red: 170 / 255, green: 86 / 255, blue: 38 / 255))
-        .cornerRadius(5)
-        .padding([.vertical])
     }
 }
 
---
+++ QuoteQuiz/QuoteQuiz/ViewControllers/GameButtonsViewController/SwiftUI/ResultsView.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsView.swift
+//  QuoteQuiz
+//
+//  Created by Zach Vilardell on 2/18/20.
+//  Copyright © 2020 zvilardell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultsView()
+    }
+}
