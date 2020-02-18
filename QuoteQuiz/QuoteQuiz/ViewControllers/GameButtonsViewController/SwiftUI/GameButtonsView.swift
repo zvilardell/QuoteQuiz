d5c34ffc21f3280105d2343860a9e8f5f592c3db
Zach Vilardell <zvilardell@bluebeam.com>
2020-02-18 12:31:43 -0800

    finalizing button ui

--- QuoteQuiz/QuoteQuiz/ViewControllers/GameButtonsViewController/SwiftUI/GameButtonsView.swift
+++ QuoteQuiz/QuoteQuiz/ViewControllers/GameButtonsViewController/SwiftUI/GameButtonsView.swift
@@ -12,27 +12,37 @@ struct GameButtonsView: View {
     var body: some View {
         ZStack {
             //set background color
-            Color.red
+            Color.black
             .edgesIgnoringSafeArea(.all)
             
             //buttons
             VStack {
-                Button(action: {
-                    print("Hey")
-                }) {
-                    Text("Yo")
-                }
+                GameButton()
+                GameButton()
+                GameButton()
             }
         }
     }
 }
 
-//struct GameButton: View {
-//    
-//    var body: some View {
-//
-//    }
-//}
+struct GameButton: View {
+
+    var body: some View {
+        Button(action: {
+            print("Hey")
+        }) {
+            Text("Yo")
+                .foregroundColor(Color.white)
+        }
+        .frame(minWidth: 0.0, maxWidth: .infinity, maxHeight: 55.0)
+        .background(Color(red: 170 / 255, green: 86 / 255, blue: 38 / 255))
+        .cornerRadius(5)
+        .padding()
+        
+        
+            
+    }
+}
 
 struct GameButtonsView_Previews: PreviewProvider {
     static var previews: some View {
