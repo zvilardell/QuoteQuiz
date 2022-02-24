4ab9d02e136b88405b893fde88d796956824fecb
Zach Vilardell <zvilardell@bluebeam.com>
2022-02-23 17:50:17 -0800

    wip, first pass generating quote with Combine

--- QuoteQuiz/QuoteQuiz/Network/HttpService.swift
+++ QuoteQuiz/QuoteQuiz/Network/HttpService.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 zvilardell. All rights reserved.
 //
 import Foundation
+import Combine
 
 class HttpService {
     
@@ -18,41 +19,53 @@ class HttpService {
     static let shared = HttpService()
     private init() {}
     
-    func getKanyeQuote(completion: @escaping (Quote)->()) {
-        get(kanyeEndpoint) { data in
-            guard let kanyeResponse = try? JSONDecoder().decode(KanyeResponse.self, from: data) else { return }
-            let quote = Quote(text: kanyeResponse.quote, quoteSource: .kanyeWest)
-            completion(quote)
-        }
+    func getKanyeQuote() -> AnyPublisher<Data, Error> {
+        return get(kanyeEndpoint)
+//        { data in
+//            guard let kanyeResponse = try? JSONDecoder().decode(KanyeResponse.self, from: data) else { return }
+//            let quote = Quote(text: kanyeResponse.quote, quoteSource: .kanyeWest)
+//            completion(quote)
+//        }
     }
     
-    func getBreakingBadQuote(completion: @escaping (Quote)->()) {
-        get(breakingBadEndpoint) { data in
-            guard let arrayResponse = try? JSONDecoder().decode(Array<BreakingBadResponse>.self, from: data), let bbResponse = arrayResponse.first else { return }
-            let quote = Quote(text: bbResponse.quote, author: bbResponse.author, quoteSource: .breakingBad)
-            completion(quote)
-        }
+    func getBreakingBadQuote() -> AnyPublisher<Data, Error> {
+        return get(breakingBadEndpoint)
+//        { data in
+//            guard let arrayResponse = try? JSONDecoder().decode(Array<BreakingBadResponse>.self, from: data), let bbResponse = arrayResponse.first else { return }
+//            let quote = Quote(text: bbResponse.quote, author: bbResponse.author, quoteSource: .breakingBad)
+//            completion(quote)
+//        }
     }
     
-    func getRonSwansonQuote(completion: @escaping (Quote)->()) {
-        get(ronSwansonEndpoint) { data in
-            guard let arrayResponse = try? JSONDecoder().decode(Array<String>.self, from: data), let ronQuote = arrayResponse.first else { return }
-            let quote = Quote(text: ronQuote, quoteSource: .ronSwanson)
-            completion(quote)
-        }
+    func getRonSwansonQuote() -> AnyPublisher<Data, Error> {
+        return get(ronSwansonEndpoint)
+//        { data in
+//            guard let arrayResponse = try? JSONDecoder().decode(Array<String>.self, from: data), let ronQuote = arrayResponse.first else { return }
+//            let quote = Quote(text: ronQuote, quoteSource: .ronSwanson)
+//            completion(quote)
+//        }
     }
     
-    private func get(_ endpoint: String, completion: @escaping (Data)->()) {
-        guard let url = URL(string: endpoint) else { return }
+    private func get(_ endpoint: String) -> AnyPublisher<Data, Error>  {
+        guard let url = URL(string: endpoint) else {
+            return Fail(error: NSError())
+                  .eraseToAnyPublisher()
+        }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
-        let task = session.dataTask(with: request) { (data, response, error) in
-            guard let responseData = data else { return }
-            completion(responseData)
-        }
+        return session.dataTaskPublisher(for: request)
+            .tryMap { data, _ in
+                return data
+            }
+            .eraseToAnyPublisher()
         
-        task.resume()
+//        let task = session.dataTask(with: request) { (data, response, error) in
+//            guard let responseData = data else { return }
+//            completion(responseData)
+//        }
+//
+//        task.resume()
     }
 }
--- QuoteQuiz/QuoteQuiz/Utilities/RandomQuoteGenerator.swift
+++ QuoteQuiz/QuoteQuiz/Utilities/RandomQuoteGenerator.swift
@@ -7,10 +7,12 @@
 //
 
 import Foundation
+import Combine
 
 class RandomQuoteGenerator {
     
     private let webService: HttpService
+    private var subscriptions: Set<AnyCancellable> = []
     
     init(webService: HttpService = .shared) {
         self.webService = webService
@@ -21,11 +23,51 @@ class RandomQuoteGenerator {
         guard let quoteSource = QuoteSource(rawValue: randomNumber) else { return }
         switch quoteSource {
         case .breakingBad:
-            webService.getBreakingBadQuote(completion: completion)
+            webService.getBreakingBadQuote()
+                .decode(type: Array<BreakingBadResponse>.self, decoder: JSONDecoder())
+//                .first()
+                .tryMap { arrayResponse in
+                    guard let bbResponse = arrayResponse.first else { return }
+                    let quote = Quote(text: bbResponse.quote, author: bbResponse.author, quoteSource: .breakingBad)
+                    completion(quote)
+                }
+                .sink {
+                    print("BB Completion: \($0)")
+                } receiveValue: {
+                    print("BB Value: \($0)")
+                }
+                .store(in: &subscriptions)
+
         case .ronSwanson:
-            webService.getRonSwansonQuote(completion: completion)
+            webService.getRonSwansonQuote()
+                .decode(type: Array<String>.self, decoder: JSONDecoder())
+//                .first()
+                .tryMap { arrayResponse in
+                    guard let ronQuote = arrayResponse.first else { return }
+                    let quote = Quote(text: ronQuote, quoteSource: .ronSwanson)
+                    completion(quote)
+                }
+                .sink {
+                    print("Ron Completion: \($0)")
+                } receiveValue: {
+                    print("Ron Value: \($0)")
+                }
+                .store(in: &subscriptions)
+            
         case .kanyeWest:
-            webService.getKanyeQuote(completion: completion)
+            webService.getKanyeQuote()
+                .decode(type: KanyeResponse.self, decoder: JSONDecoder())
+//                .first()
+                .tryMap { kanyeResponse in
+                    let quote = Quote(text: kanyeResponse.quote, quoteSource: .kanyeWest)
+                    completion(quote)
+                }
+                .sink {
+                    print("Kanye Completion: \($0)")
+                } receiveValue: {
+                    print("Kanye Value: \($0)")
+                }
+                .store(in: &subscriptions)
         }
     }
 }
